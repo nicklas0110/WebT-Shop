6d9a7dcca85d6f9561e78fb7bcf6298c6ff81945
FrederikReigstad <Frederik9876@gmail.com>
2022-12-09 14:43:29 +0100

    forntend new set up

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,10 +7,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
 import {MatCardModule} from "@angular/material/card";
+import { LoginComponent } from './login/login.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { SuperadminComponent } from './superadmin/superadmin.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import {Routes} from "@angular/router";
+
+const route: Routes = [
+  {
+    path: 'userpage', component: UserpageComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'adminpage', component: AdminpageComponent
+  },
+  {
+    path: 'superadmin', component: SuperadminComponent
+  }
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserpageComponent,
+    SuperadminComponent,
+    AdminpageComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/services/http.service.ts
+++ frontend/src/services/http.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import axios from "axios";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {catchError} from "rxjs";
 
 export const customAxios = axios.create({
   baseURL: 'http://localhost5111'
@@ -10,15 +12,37 @@ export const customAxios = axios.create({
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(){
+    /*customAxios.interceptors.response.use(
+    response => {
+      if(response.status == 201){
+        this.matSnackbar.open("Great Success")
+      }
+      return response
+    },
+    rejected => {
+      if (rejected.response.status>= 400 && rejected.response.status <500){
+        matSnackbar.open(rejected.response.data);
+      }else if (rejected.response.status>499){
+        this.matSnackbar.open("Something Went Wrong")
+      }
+      catchError(rejected)
+    }) */
+  }
 
   async getOption(){
     const httpResponse = await customAxios.get('option');
     return httpResponse.data;
   }
 
-  async createBox(dto: {name: any;}) {
+  async createOption(dto: {name: any;}) {
     const httpResponse = await customAxios.post('option',dto)
+    return httpResponse.data;
+  }
+
+  // puts to the backend using an id and the dto class whits also contain an id and edits
+  async editOption(id : any, dto: { size: any; type: any; customerName: any }) {
+    const httpResponse = await customAxios.put('box/' + id, dto)
     return httpResponse.data;
   }
 }
