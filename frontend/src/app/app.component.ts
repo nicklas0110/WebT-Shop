1fe8e192b703e1f3cb908b660172bd1d1b06633b
FrederikReigstad <Frederik9876@gmail.com>
2022-12-09 13:08:10 +0100

    new frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,33 +1,10 @@
+import { Component } from '@angular/core';
 
-import {HttpService} from "../services/http.service";
-
-/*@Component({
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']*/
-/*})*/
+  styleUrls: ['./app.component.scss']
+})
 export class AppComponent {
-  formModel : Option = new Option(); // Sets formModel = to the Option class
-
-  constructor(private http: HttpService) {
-  }
-
-
-
-  async ngOnInit(){
-    const options = await this.http.getOptions();
-    console.log(options)
-  }
-
-
-}
-
-
-// is in change of the 3 txt fields information storing when creating or edit
-class OptionDto {
-  name: string = "";
-}
-// Sets the id when it is needed
-class Option extends OptionDto{
-  id: number = 0;
+  title = 'frontend';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
