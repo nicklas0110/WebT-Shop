5063423dcadfde4bc58093e8175dd3fe676c0f23
FrederikReigstad <Frederik9876@gmail.com>
2022-12-09 13:25:23 +0100

    it works

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  formModel: Option = new Option; // Sets formModel = to the Option class
+  option: any;
+  options: Array<Option> = []; // an array of Options
+
+  ngOnInit(): void {
+  }
+}
+
+// is in change of the 3 txt fields information storing when creating or edit
+class OptionDto {
+  name: string = "";
+}
+// Sets the id when it is needed
+class Option extends OptionDto{
+  id: number = 0;
 }
