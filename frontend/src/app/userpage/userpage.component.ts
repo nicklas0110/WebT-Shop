5d29815208a5b8970b75a374c851e1dc62ce8d75
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-12-15 01:53:34 +0100

    userpage start

--- frontend/src/app/userpage/userpage.component.ts
+++ frontend/src/app/userpage/userpage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../../services/http.service";
 import {customAxios} from "../app.component";
 
+
 @Component({
   selector: 'app-userpage',
   templateUrl: './userpage.component.html',
@@ -9,16 +10,29 @@ import {customAxios} from "../app.component";
 })
 
 export class UserpageComponent implements OnInit {
+  formModel : Option = new Option(); // Sets formModel = to the Box class
+  option: any;
+  options: any[] = [];
 
+  constructor(private http : HttpService) { }
 
-  constructor(private http: HttpService) {
-
+  async ngOnInit(){
+    const options = await this.http.getOption();
+    this.options = options;
+    this.option = options;
   }
 
-  async ngOnInit() {
 
-
+  selectCard(option: Option) {
+    this.formModel = {...option};
   }
+}
 
-
+class OptionDto {
+  optionName: string = "";
+  optionGroupId: number = 1;
+}
+// Sets the id when it is needed
+class Option extends OptionDto{
+  id: number = 0;
 }
